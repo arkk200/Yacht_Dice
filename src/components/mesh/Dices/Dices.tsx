77348c7191af6460a93375ef0d5cecf3da429ea8
이명재 <arkk20000@gmail.com>
2023-07-22 23:20:06 +0900

    FIX :: 주사위들 겹치지 않게 위치 변경

--- src/components/mesh/Dices/Dices.tsx
+++ src/components/mesh/Dices/Dices.tsx
@@ -3,11 +3,11 @@ import Dice from "../Dice/Dice";
 const Dices = () => {
   return (
     <>
-      <Dice />
-      <Dice />
-      <Dice />
-      <Dice />
-      <Dice />
+      <Dice position={[0, 1.5, 0]} />
+      <Dice position={[-0.3, 0.5, 0.3]} />
+      <Dice position={[0.3, 1, 0.3]} />
+      <Dice position={[0, 1, -0.6]} />
+      <Dice position={[0.6, 0.5, 0]} />
     </>
   );
 };
