d10f7e24d32aa7087e4450ca5a644622ddd5eed0
이명재 <arkk20000@gmail.com>
2023-07-30 15:01:55 +0900

    ADD :: 주사위 흔드는 기능 useShakeDices 훅으로 뺌

--- src/components/r3f/DiceCup/hooks/useHandleDiceCup.hook.ts
+++ src/components/r3f/DiceCup/hooks/useHandleDiceCup.hook.ts
@@ -1,78 +1,20 @@
-import { useDocumentEventListener } from "@/providers/DocumentEventListenerProvider/DocumentEventListenerProvider.hooks";
-import { useWindowEventListener } from "@/providers/WindowEventListenerProvider/WindowEventListenerProvider.hooks";
 import { gameStatusState } from "@/stores/game.store";
 import { RapierRigidBody } from "@react-three/rapier";
 import gsap from "gsap";
 import { RefObject, useEffect } from "react";
 import { useRecoilValue } from "recoil";
-import { Euler, Quaternion, Vector3 } from "three";
+import { Euler, Quaternion } from "three";
+import useShakeDices from "./useShakeDices";
 
 const useHandleDiceCup = (
   diceCupRigidBodyRef: RefObject<RapierRigidBody | null>
 ) => {
   const gameStatus = useRecoilValue(gameStatusState);
 
-  const { addWindowEventListener, removeWindowEventListener } =
-    useWindowEventListener();
-
-  const { addDocumentEventListener, removeDocumentEventListener } =
-    useDocumentEventListener();
+  const { shakeDices, removeShakeDicesEvents } =
+    useShakeDices(diceCupRigidBodyRef);
 
   useEffect(() => {
-    const 주사위흔들기 = () => {
-      let center: { x: number; y: number } | null = null;
-
-      let x: number = 0;
-      let y: number = 0;
-
-      const handleTransformedPosition = (e: MouseEvent) => {
-        if (!center) center = { x: e.clientX, y: e.clientY };
-
-        x = e.clientX - center.x;
-        y = e.clientY - center.y;
-
-        const limitPosition = 150;
-
-        if (Math.abs(x) > limitPosition) {
-          center.x = e.clientX - limitPosition * Math.sign(x);
-          x = Math.sign(x) * limitPosition;
-        }
-        if (Math.abs(y) > limitPosition) {
-          center.y = e.clientY - limitPosition * Math.sign(y);
-          y = Math.sign(y) * limitPosition;
-        }
-      };
-
-      addWindowEventListener(
-        "handleDiceCupMove",
-        "mousemove",
-        (e: MouseEvent) => {
-          const diceCupRigidBody = diceCupRigidBodyRef.current;
-          if (!diceCupRigidBody) return;
-
-          handleTransformedPosition(e);
-
-          const sensityDivideNumber = 1000;
-
-          diceCupRigidBody.setNextKinematicTranslation(
-            new Vector3(
-              1.75 + x / sensityDivideNumber,
-              1.5 + Math.sin((x + y) / 2000),
-              y / sensityDivideNumber
-            )
-          );
-        }
-      );
-
-      addDocumentEventListener("handleMouseLeave", "mouseleave", () => {
-        center = null;
-      });
-      addWindowEventListener("handleBlur", "blur", () => {
-        center = null;
-        console.log("blur");
-      });
-    };
-
     const 주사위굴리기 = () => {
       const curEuler = new Euler(0, 0, 0);
 
@@ -90,23 +32,12 @@ const useHandleDiceCup = (
 
     switch (gameStatus) {
       case "주사위흔들기":
-        주사위흔들기();
-        return () => {
-          removeWindowEventListener("handleDiceCupMove");
-          removeDocumentEventListener("handleMouseLeave");
-          removeWindowEventListener("handleBlur");
-        };
+        shakeDices();
+        return removeShakeDicesEvents;
       case "주사위굴리기":
         주사위굴리기();
     }
-  }, [
-    diceCupRigidBodyRef,
-    gameStatus,
-    addWindowEventListener,
-    removeWindowEventListener,
-    addDocumentEventListener,
-    removeDocumentEventListener,
-  ]);
+  }, [diceCupRigidBodyRef, gameStatus, removeShakeDicesEvents, shakeDices]);
 };
 
 export default useHandleDiceCup;
